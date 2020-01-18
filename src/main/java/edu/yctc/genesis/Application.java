4566fc5e8ecc7408255560abc74c94d62fba0ad3
improve <15696756582@163.com>
2020-01-18 23:43:01 +0800

    增加sql文件和相关发行版本控制

--- src/main/java/edu/yctc/genesis/Application.java
+++ src/main/java/edu/yctc/genesis/Application.java
@@ -3,6 +3,8 @@ package edu.yctc.genesis;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,9 +13,15 @@ import org.springframework.web.bind.annotation.RestController;
 @MapperScan("edu.yctc.genesis.*") //开启mapper接口扫描
 @EnableScheduling
 
-public class Application {
+public class Application extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
+
 }
